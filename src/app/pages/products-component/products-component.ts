6dca11a13cd69b2967f6ae9179c704c92742a071
Mena Erian <mena3koko2karas1@gmail.com>
2025-07-25 21:52:43 +0300

    feat: develop product display with cardProduct component and integrate product service for dynamic data

--- src/app/pages/products-component/products-component.ts
+++ src/app/pages/products-component/products-component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  inject,
+  OnInit,
+  signal,
+  WritableSignal,
+} from '@angular/core';
+import { CardProductComponent } from '../../shared/components/card-product-component/card-product-component';
+import { FormsModule } from '@angular/forms';
+import { ProductService } from '../../shared/services/product/product-service';
+import { IProduct } from '../../core/interfaces/Iproducts';
 
 @Component({
   selector: 'app-products-component',
-  imports: [],
+  imports: [CardProductComponent, FormsModule],
   templateUrl: './products-component.html',
-  styleUrl: './products-component.scss'
+  styleUrl: './products-component.scss',
 })
-export class ProductsComponent {
-
+export class ProductsComponent implements OnInit {
+  productService = inject(ProductService);
+  products: WritableSignal<IProduct[]> = signal<IProduct[]>([]);
+  ngOnInit(): void {
+    this.productService.getAllProducts().subscribe({
+      next: (res) => {
+        console.log(res);
+        this.products.set(res);
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
+  }
 }
