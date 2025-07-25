017a688c69a8819286e142f5ddf1361d051be180
Mena Erian <mena3koko2karas1@gmail.com>
2025-07-25 17:55:53 +0300

    Initial project setup: installed required libraries and created folder structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FlowbiteService } from './shared/services/flowbite/flowbite-service';
+import { initFlowbite } from 'flowbite';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
+export class App implements OnInit {
   protected title = 'ProductsGallery';
+  constructor(private flowbiteService: FlowbiteService) {}
+
+  ngOnInit(): void {
+    this.flowbiteService.loadFlowbite((flowbite) => {
+      initFlowbite();
+    });
+  }
 }
