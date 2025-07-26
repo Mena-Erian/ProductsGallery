a6dfb08256d37d6d029d4d226d551f1abff28dcd
Mena Erian <mena3koko2karas1@gmail.com>
2025-07-26 20:50:17 +0300

    feat: implemented Product rating display with logic in ProductCard and Product Details components

--- src/app/pages/product-details-component/product-details-component.ts
+++ src/app/pages/product-details-component/product-details-component.ts
@@ -9,10 +9,12 @@ import {
 import { ProductService } from '../../shared/services/product/product-service';
 import { ActivatedRoute } from '@angular/router';
 import { IProduct } from '../../core/interfaces/Iproducts';
+import { FormsModule } from '@angular/forms';
+import { Rating } from 'primeng/rating';
 
 @Component({
   selector: 'app-product-details-component',
-  imports: [CurrencyPipe],
+  imports: [CurrencyPipe, FormsModule, Rating],
   templateUrl: './product-details-component.html',
   styleUrl: './product-details-component.scss',
 })
@@ -23,7 +25,7 @@ export class ProductDetailsComponent implements OnInit {
     null
   );
   productId: string = '';
-
+  rattingValue: number = 0;
   ngOnInit(): void {
     this.getProductDetails();
   }
@@ -43,6 +45,7 @@ export class ProductDetailsComponent implements OnInit {
     this.productService.getSingleProduct(this.getParamProductId()).subscribe({
       next: (res) => {
         this.productDetails.set(res);
+        this.rattingValue = this.productDetails()?.rating.rate ?? 0;
       },
       error: (err) => {
         console.error(err);
--- src/app/shared/components/card-product-component/card-product-component.ts
+++ src/app/shared/components/card-product-component/card-product-component.ts
@@ -1,16 +1,26 @@
-import { Component, input } from '@angular/core';
+import { Component, effect, input } from '@angular/core';
 import { IProduct } from '../../../core/interfaces/Iproducts';
 import { CurrencyPipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
-// import { CurrencyPipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Rating } from 'primeng/rating';
 
 @Component({
   selector: 'app-card-product-component',
-  imports: [CurrencyPipe, RouterLink],
+  imports: [CurrencyPipe, RouterLink, FormsModule, Rating],
   templateUrl: './card-product-component.html',
   styleUrl: './card-product-component.scss',
 })
 export class CardProductComponent {
   test: IProduct = {} as IProduct;
   product = input<IProduct>();
+
+  rattingValue: number = 0;
+
+  constructor() {
+    // effect in any injection context
+    effect(() => {
+      this.rattingValue = this.product()?.rating.rate ?? 0;
+    });
+  }
 }
