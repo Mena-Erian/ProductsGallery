d3c03269d8c8fa653bda7def5ee60bfca8edede2
Mena Erian <mena3koko2karas1@gmail.com>
2025-07-27 22:34:16 +0300

    feat(cart): implement add to cart functionality, update product card component, and enhance cart service for product management

--- src/app/shared/components/card-product-component/card-product-component.ts
+++ src/app/shared/components/card-product-component/card-product-component.ts
@@ -1,9 +1,13 @@
-import { Component, effect, input } from '@angular/core';
+import { Component, effect, inject, input, OnInit } from '@angular/core';
 import { IProduct } from '../../../core/interfaces/Iproducts';
 import { CurrencyPipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { Rating } from 'primeng/rating';
+import { CartService } from '../../services/cart/cart-service';
+import { AuthService } from '../../services/auth/auth-service';
+import { MessageService } from 'primeng/api';
+// import { Toast } from 'primeng/toast';
 
 @Component({
   selector: 'app-card-product-component',
@@ -11,16 +15,47 @@ import { Rating } from 'primeng/rating';
   templateUrl: './card-product-component.html',
   styleUrl: './card-product-component.scss',
 })
-export class CardProductComponent {
-  test: IProduct = {} as IProduct;
+export class CardProductComponent implements OnInit {
+  private readonly cartService = inject(CartService);
+  private readonly authService = inject(AuthService);
+  private readonly msg = inject(MessageService);
+  test: any = {} as IProduct;
   product = input<IProduct>();
-
+  cartId: string = '';
   rattingValue: number = 0;
-
   constructor() {
     // effect in any injection context
     effect(() => {
       this.rattingValue = this.product()?.rating.rate ?? 0;
+      this.test = this.product();
     });
+  }
+  ngOnInit(): void {
+    this.cartService
+      .getSingleCartItem(this.authService.user().id.toString())
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.cartId = res.id;
+        },
+      });
+  }
+  addToCart() {
+    this.cartService
+      .addProductToCart({
+        id: +this.cartId,
+        userId: this.authService.user().id,
+        products: this.product() != undefined ? [this.product()] : [this.test],
+      })
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.msg.add({
+            severity: 'success',
+            summary: 'success',
+            detail: `Add ${res.products[0].title} to your Cart`,
+          });
+        },
+      });
   }
 }
--- src/app/shared/services/cart/cart-service.ts
+++ src/app/shared/services/cart/cart-service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, signal, WritableSignal } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment.development';
+import { IProduct } from '../../../core/interfaces/Iproducts';
 
 @Injectable({
   providedIn: 'root',
@@ -12,10 +13,12 @@ export class CartService {
   getAllCart(): Observable<any> {
     return this.httpClient.get<any>(`${environment.baseUrl}/carts`);
   }
-  addProductToCart(id: string): Observable<any> {
-    return this.httpClient.post<any>(`${environment.baseUrl}/carts`, {
-      productId: id,
-    });
+  addProductToCart(data: {
+    id: number;
+    userId: number;
+    products: IProduct[];
+  }): Observable<any> {
+    return this.httpClient.post<any>(`${environment.baseUrl}/carts`, data);
   }
   getSingleCartItem(id: string): Observable<any> {
     return this.httpClient.delete(`${environment.baseUrl}/carts/${id}`);
